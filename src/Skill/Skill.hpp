32b60341946aa0f1884a058d526be6229e363faf
michael-owen-01 <80764250+michael-owen-01@users.noreply.github.com>
2021-03-22 23:40:58 +0700

    Update Skill.hpp

--- src/Skill/Skill.hpp
+++ src/Skill/Skill.hpp
@@ -2,8 +2,8 @@
 #define __SKILL_HPP__
 
 #include<string>
-#include "../Element/Element.hpp"
-
+#include<vector>
+#include"Element.hpp"
 using namespace std;
 
 class Skill{
@@ -11,13 +11,134 @@ public:
     Skill();
     Skill(int, int, Element);
     ~Skill();
-    getBasePower()
+    int getBasePower();
     
 protected:
     int basePower;
     int masteryLevel;
-    Element* prereqElmt;
+    vector<Element*> prereqElmt;
 };
 
+class skillItem{
+public:
+    skillItem();
+    skillItem(int);
+    virtual void learn();
+    int getJumlahSkill();
+    void useSkill();
+protected:
+    int jumlah;
+    
+};
+
+class Blaze : public Skill{
+public:
+    Blaze();
+};
+
+class innerFocus : public Skill{
+public:
+    innerFocus();
+};
+
+class lightningRod : public Skill{
+public:
+    lightningRod();
+};
+
+class Pressure : public Skill{
+public:
+    Pressure();
+};
+
+class sandForce : public Skill{
+public:
+    sandForce();
+};
+
+class Drought : public Skill{
+public:
+    Drought();
+}
+
+class rainDish : public Skill{
+public:
+    rainDish();
+}
+
+class Moxie : public Skill{
+public:
+    Moxie();
+}
+
+class iceBody : public Skill{
+public:
+    iceBody();
+}
+
+class snowCloak : public Skill{
+public:
+    snowCloak();
+}
+
+class BlazeSkill : public Skill, public skillItem{
+public:
+    BlazeSkill();
+    learn();
+};
+
+class innerFocusSkill : public Skill, public skillItem{
+public:
+    innerFocusSkill();
+    learn();
+};
+
+class lightningRodSkill : public Skill, public skillItem{
+public:
+    lightningRodSkill();
+    learn();
+};
+
+class PressureSkill : public Skill, public skillItem{
+public:
+    PressureSkill();
+    learn();
+};
+
+class sandForceSkill : public Skill, public skillItem{
+public:
+    sandForceSkill();
+    learn();
+};
+
+class DroughtSkill : public Skill, public skillItem{
+public:
+    DroughtSkill();
+    learn();
+}
+
+class rainDishSkill : public Skill, public skillItem{
+public:
+    rainDishSkill();
+    learn();
+}
+
+class MoxieSkill : public Skill, public skillItem{
+public:
+    MoxieSkill();
+    learn();
+}
+
+class iceBodySkill : public Skill, public skillItem{
+public:
+    iceBodySkill();
+    learn();
+}
+
+class snowCloakSkill : public Skill, public skillItem{
+public:
+    learn();
+    snowCloakSkill();
+}
 
 #endif
